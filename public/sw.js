2a8a8a131878ed03943a748fd4ab0b605e03f1d4
Kyle Geske <stungeye@gmail.com>
2025-07-07 12:53:12 -0500

    Manifest and service work caching (state while revalidate) have been added along with 192x192 and 512x512 icons. This should allow for offline usage, as well as home screen display without browser chrome/url-bar.

---
+++ public/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "spaced-out-app-v2"; // Increment cache version
+const STATIC_ASSETS = ["/", "/index.html", "/icon192.png", "/icon512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      // Pre-cache the main static assets
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  // Clean up old caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(async (cache) => {
+      // Try to get the response from the cache
+      const cachedResponse = await cache.match(event.request);
+      // Fetch from the network in the background
+      const fetchedResponsePromise = fetch(event.request).then(
+        (networkResponse) => {
+          // If the fetch is successful, update the cache
+          if (networkResponse && networkResponse.status === 200) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }
+      );
+
+      // Return the cached response if it exists, otherwise wait for the network
+      return cachedResponse || fetchedResponsePromise;
+    })
+  );
+});
